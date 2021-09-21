192b1ca8be7dc363e24352bd02d6464290c9be16
dizzi <nicofirst1@gmail.com>
2021-09-21 11:48:10 +0200

    removed tags

--- NavigationEnv.py
+++ NavigationEnv.py
@@ -18,31 +18,26 @@ def get_env():
 
 
 class Scenario(BaseScenario):
-    def make_world(self, num_good=1, num_adversaries=3, num_obstacles=2):
+    def make_world(self, num_agents=1,  num_landmarks=2):
         world = World()
         # set any world properties first
         world.dim_c = 2
-        num_good_agents = num_good
-        num_adversaries = num_adversaries
-        num_agents = num_adversaries + num_good_agents
-        num_landmarks = num_obstacles
         # add agents
-        world.agents = [Agent() for i in range(num_agents)]
+        world.agents = [Agent() for _ in range(num_agents)]
         for i, agent in enumerate(world.agents):
-            agent.adversary = True if i < num_adversaries else False
-            base_name = "adversary" if agent.adversary else "agent"
-            base_index = i if i < num_adversaries else i - num_adversaries
-            agent.name = f"{base_name}_{base_index}"
+            agent.name = f"agent_{i}"
             agent.collide = True
             agent.silent = True
-            agent.size = 0.075 if agent.adversary else 0.05
-            agent.accel = 3.0 if agent.adversary else 4.0
-            agent.max_speed = 1.0 if agent.adversary else 1.3
+            agent.size =  0.05
+            agent.accel =  4.0
+            agent.max_speed =  1.3
+
+
         # add landmarks
         world.landmarks = [Landmark() for i in range(num_landmarks)]
         for i, landmark in enumerate(world.landmarks):
             landmark.name = "landmark %d" % i
-            landmark.collide = True
+            landmark.collide = False
             landmark.movable = False
             landmark.size = 0.2
             landmark.boundary = False
@@ -51,11 +46,8 @@ class Scenario(BaseScenario):
     def reset_world(self, world, np_random):
         # random properties for agents
         for i, agent in enumerate(world.agents):
-            agent.color = (
-                np.array([0.35, 0.85, 0.35])
-                if not agent.adversary
-                else np.array([0.85, 0.35, 0.35])
-            )
+            agent.color =  np.array([0.85, 0.35, 0.35])
+
             # random properties for landmarks
         for i, landmark in enumerate(world.landmarks):
             landmark.color = np.array([0.25, 0.25, 0.25])
@@ -69,16 +61,6 @@ class Scenario(BaseScenario):
                 landmark.state.p_pos = np_random.uniform(-0.9, +0.9, world.dim_p)
                 landmark.state.p_vel = np.zeros(world.dim_p)
 
-    def benchmark_data(self, agent, world):
-        # returns data for benchmarking purposes
-        if agent.adversary:
-            collisions = 0
-            for a in self.good_agents(world):
-                if self.is_collision(a, agent):
-                    collisions += 1
-            return collisions
-        else:
-            return 0
 
     def is_collision(self, agent1, agent2):
         delta_pos = agent1.state.p_pos - agent2.state.p_pos
@@ -87,38 +69,12 @@ class Scenario(BaseScenario):
         return True if dist < dist_min else False
 
     # return all agents that are not adversaries
-    def good_agents(self, world):
-        return [agent for agent in world.agents if not agent.adversary]
+    def get_agents(self, world):
+        return [agent for agent in world.agents]
 
-    # return all adversarial agents
-    def adversaries(self, world):
-        return [agent for agent in world.agents if agent.adversary]
 
     def reward(self, agent, world):
-        # Agents are rewarded based on minimum agent distance to each landmark
-        main_reward = (
-            self.adversary_reward(agent, world)
-            if agent.adversary
-            else self.agent_reward(agent, world)
-        )
-        return main_reward
-
-    def agent_reward(self, agent, world):
-        # Agents are negatively rewarded if caught by adversaries
         rew = 0
-        shape = False
-        adversaries = self.adversaries(world)
-        if (
-                shape
-        ):  # reward can optionally be shaped (increased reward for increased distance from adversary)
-            for adv in adversaries:
-                rew += 0.1 * np.sqrt(
-                    np.sum(np.square(agent.state.p_pos - adv.state.p_pos))
-                )
-        if agent.collide:
-            for a in adversaries:
-                if self.is_collision(a, agent):
-                    rew -= 10
 
         # agents are penalized for exiting the screen, so that they can be caught by the adversaries
         def bound(x):
@@ -134,26 +90,6 @@ class Scenario(BaseScenario):
 
         return rew
 
-    def adversary_reward(self, agent, world):
-        # Adversaries are rewarded for collisions with agents
-        rew = 0
-        shape = False
-        agents = self.good_agents(world)
-        adversaries = self.adversaries(world)
-        if (
-                shape
-        ):  # reward can optionally be shaped (decreased reward for increased distance from agents)
-            for adv in adversaries:
-                rew -= 0.1 * min(
-                    np.sqrt(np.sum(np.square(a.state.p_pos - adv.state.p_pos)))
-                    for a in agents
-                )
-        if agent.collide:
-            for ag in agents:
-                for adv in adversaries:
-                    if self.is_collision(ag, adv):
-                        rew += 10
-        return rew
 
     def observation(self, agent, world):
         # get positions of all entities in this agent's reference frame
@@ -170,8 +106,7 @@ class Scenario(BaseScenario):
                 continue
             comm.append(other.state.c)
             other_pos.append(other.state.p_pos - agent.state.p_pos)
-            if not other.adversary:
-                other_vel.append(other.state.p_vel)
+            other_vel.append(other.state.p_vel)
         return np.concatenate(
             [agent.state.p_vel]
             + [agent.state.p_pos]
@@ -186,4 +121,4 @@ class raw_env(SimpleEnv):
         scenario = Scenario()
         world = scenario.make_world(N)
         super().__init__(scenario, world, max_cycles, continuous_actions)
-        self.metadata["name"] = "simple_adversary_v2"
+        self.metadata["name"] = "collab_nav"
