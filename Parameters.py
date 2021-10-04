60d7eb3d41bcd722f5942623649b2bdb837725c8
dizzi <nicofirst1@gmail.com>
2021-10-04 16:13:50 +0200

    reformat and modularized

---
+++ Parameters.py
@@ -0,0 +1,18 @@
+import os
+
+
+class Params:
+
+
+    #### TRAINING params
+
+    num_workers = 6
+    num_gpus = int(os.environ.get("RLLIB_NUM_GPUS", "0"))
+    framework = "tf"
+    debug = False
+
+    #### ENV params
+    agents = 2
+    landmarks = 3
+    experiment_name = "collab_nav"
+    model_name = f"{experiment_name}_model"
--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import os
-
 import ray
 from ray.rllib.models import ModelCatalog
 from ray.tune.logger import pretty_print
@@ -7,50 +5,74 @@ from ray.tune.registry import register_env
 
 from NavigationEnv import get_env
 from NavigationModel import NavModel
+from Parameters import Params
 
-if __name__ == "__main__":
-    debug = True
-    agents = 2
-    experiment_name = "collab_nav"
-    model_name = f"{experiment_name}_model"
 
-    ray.init(local_mode=True)
-
+def get_env_configs(params: Params):
     ## ENV CONFIG
     env_config = dict(
-        N=agents, landmarks=3, max_cycles=25, continuous_actions=False, name=experiment_name
+        N=params.agents, landmarks=params.landmarks, max_cycles=25, continuous_actions=False,
+        name=params.experiment_name
     )
 
-    register_env(experiment_name, lambda config: get_env(config))
+    register_env(params.experiment_name, lambda config: get_env(config))
 
-    ## POLICY CONFIG
+    return env_config
+
+
+def get_policy_configs(params: Params):
     env = get_env(env_config)
 
-    model_configs = dict(custom_model=model_name,
-                         vf_share_layers=True, )
-    ModelCatalog.register_custom_model(model_name, NavModel)
+    ModelCatalog.register_custom_model(params.model_name, NavModel)
 
     policies = {agnt: (None, env.observation_space, env.action_space, {}) for agnt in env.agents}
-    multiagent_config = dict(
+    policy_configs = dict(
         policies=policies,
         policy_mapping_fn=lambda agent_id: agent_id
     )
 
-    ## MODEL CONFIG
+    return policy_configs
 
-    model_configs = dict(custom_model=model_name,
+
+def get_model_configs(params: Params):
+    model_configs = dict(custom_model=params.model_name,
                          vf_share_layers=True, )
-    ModelCatalog.register_custom_model(model_name, NavModel)
+    ModelCatalog.register_custom_model(params.model_name, NavModel)
+    return model_configs
 
-    config = {"env": experiment_name, "simple_optimizer": True, "num_gpus": int(os.environ.get("RLLIB_NUM_GPUS", "0")),
-              "num_workers": 1 if not debug else 0, "framework": "tf", 'model': model_configs,
-              'env_config': env_config, "multiagent": multiagent_config, }
+
+def get_general_configs(params: Params):
+    configs = dict(
+        env=params.experiment_name,
+        num_gpus=params.num_gpus if not params.debug else 0,
+        num_workers=params.num_workers if not params.debug else 0,
+        framework=params.framework,
+    )
+
+    return configs
+
+
+if __name__ == "__main__":
+    params = Params()
+
+    ray.init(local_mode=params.debug)
+
+    # Get configs
+    env_config = get_env_configs(params)
+    policy_configs = get_policy_configs(params)
+    model_configs = get_model_configs(params)
+    configs = get_general_configs(params)
+
+    # set specific configs in general dic
+    configs['model'] = model_configs
+    configs['env_config'] = env_config
+    configs['multiagent'] = policy_configs
 
     ## TRAIING
 
     analysis = ray.tune.run(
         "PPO",
-        name=f"{experiment_name}_test",
+        name=f"{params.experiment_name}_test",
         metric="episode_reward_mean",
-        config=config)
+        config=configs)
     print(analysis)
