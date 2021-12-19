fixed action_log_probs calculation

src/common/Params.py
@@ -25,7 +25,7 @@ class Params:
     num_workers = multiprocessing.cpu_count() - 1
     num_gpus = torch.cuda.device_count()
     framework = "torch"
-    minibatch = 128
+    minibatch = 2
     epochs = 1000
 
     ### Optimizer
@@ -45,7 +45,7 @@ class Params:
     #### ENVIRONMENT ####
     agents = 1
     landmarks = 1
-    horizon = 64
+    horizon = 5
     episodes = 3
     env_name = "collab_nav"
     model_name = f"{env_name}_model"

src/model/ModelFree.py
@@ -41,7 +41,7 @@ class OnPolicy(nn.Module):
 
         return action
 
-    def evaluate_actions(self, frames: torch.Tensor, action_indx: torch.Tensor):
+    def evaluate_actions(self, frames: torch.Tensor, actions_indxs: torch.Tensor):
         """evaluate_actions method.
 
         compute the actions logit, value and actions probability by passing
@@ -70,14 +70,14 @@ class OnPolicy(nn.Module):
             value of the entropy given by the action with index equal to
             action_indx.
         """
-        action_logit, value = self.forward(frames)
+        action_logit, value_logit = self.forward(frames)
+
         action_probs = F.softmax(action_logit, dim=1)
-        log_probs = F.log_softmax(action_logit, dim=1)
+        action_probs_log = F.log_softmax(action_logit, dim=1)
 
-        action_log_prob = log_probs.gather(1, action_indx)
-        entropy = -(action_probs * log_probs).sum(1).mean()
+        entropy = -(action_probs * action_probs_log).sum(1).mean()
 
-        return action_logit, action_log_prob, action_probs, value, entropy
+        return action_logit, action_probs_log.gather(1, actions_indxs.unsqueeze(dim=1)), action_probs, value_logit, entropy
 
 
 class ModelFree(OnPolicy):

src/model/RolloutStorage.py
@@ -20,7 +20,7 @@ class RolloutStorage(object):
         self.values = torch.zeros(num_steps, num_agents)
         self.returns = torch.zeros(num_steps, num_agents)
         self.gae = torch.zeros(num_steps, num_agents)
-        self.action_log_probs = torch.zeros(num_steps, num_agents, num_actions)
+        self.action_log_probs = torch.zeros(num_steps, num_agents)
 
     def to(self, device):
         self.states = self.states.to(device)

src/train/Policies.py
@@ -85,9 +85,9 @@ class MultimodalMAS(TrajCollectionPolicy):
         action_logit, value_logit = self.ac_dict[agent_id](observation)
         action_probs = F.softmax(action_logit, dim=1)
         action = action_probs.multinomial(1).squeeze()
-        action_logs = F.log_softmax(action_probs).squeeze()
+        action_probs_log = F.log_softmax(action_logit, dim=1).squeeze()
 
         #value = int(value_logit)
         action = int(action)
 
-        return action, value_logit, action_logs[action]
+        return action, value_logit, action_probs_log[action]

src/train/train_utils.py
@@ -23,7 +23,11 @@ def mas_dict2tensor(agent_dict, type) -> torch.Tensor:
     """
 
     tensor = sorted(agent_dict.items())
-    tensor = [type(elem[1]) for elem in tensor]
+    if type is not None:
+        tensor = [type(elem[1]) for elem in tensor]
+    else:
+        tensor = [elem[1] for elem in tensor]
+
     return torch.as_tensor(tensor)
 
 # todo: this can be done in parallel
@@ -85,7 +89,6 @@ def collect_trajectories(
 
                 action_dict[agent_id] = action
                 values_dict[agent_id] = value
-
                 action_log_dict[agent_id] = action_log
 
             # Our reward/dones are dicts {'agent_0': val0,'agent_1': val1}
@@ -99,8 +102,8 @@ def collect_trajectories(
             masks = 1 - mas_dict2tensor(dones, int)
             rewards = mas_dict2tensor(rewards, int)
             actions = mas_dict2tensor(action_dict, int)
-            values = mas_dict2tensor(values_dict, int)
-            action_log = action_log_dict.unsqueeze(dim=0)
+            values = mas_dict2tensor(values_dict, float)
+            action_log = mas_dict2tensor(action_log_dict, float)
 
             current_state = next_state.to(params.device)
             observation = observation.squeeze(dim=0)
@@ -143,14 +146,14 @@ def train_epoch_PPO(
 
     """
 
-    rollout.compute_returns(rollout.values[-1])
+    rollout.compute_returns()
     rollout.to(params.device)
 
     # # get data generation that splits rollout in batches
     data_generator = rollout.recurrent_generator()
     infos = dict(
         value_loss=[],
-        action_loss=[],
+        surrogate_loss=[],
         entropys=[],
         loss=[]
     )
@@ -177,12 +180,10 @@ def train_epoch_PPO(
 
         for agent_id in env.agents:
             agent_index = env.agents.index(agent_id)
-            agent_action = actions_minibatch[:, agent_index]
+            agent_actions = actions_minibatch[:, agent_index]
 
             agent = ac_dict[agent_id]
-            _, action_logs, action_probs, values, entropys = agent.evaluate_actions(
-                states_minibatch, agent_action
-            )
+            _, action_logs, action_probs, values, entropys = agent.evaluate_actions(states_minibatch, agent_actions)
 
             loss, value_loss, surrogate_loss = compute_PPO_update(agent, optimizer, return_minibatch, values, action_logs,
                 old_action_logs_minibatch, adv_targ_minibatch, entropys, params)