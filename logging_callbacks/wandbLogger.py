fix model free and input normalization

logging_callbacks/wandbLogger.py
@@ -142,27 +142,26 @@ class PPOWandb(WandbLogger):
 
         logs["epoch"] = batch_id
 
-        if batch_id % self.log_behavior_step == 0:
-            # done_idx = (rollout.masks == 0).nonzero(as_tuple=True)[0].cpu()
-            #
-            # if len(done_idx) > 1:
-            #     done_idx = done_idx[0]
+        # done_idx = (rollout.masks == 0).nonzero(as_tuple=True)[0].cpu()
+        #
+        # if len(done_idx) > 1:
+        #     done_idx = done_idx[0]
 
-            states = (
-                rollout.states[:rollout.step][:, -3:, :, :].cpu().numpy().astype(np.uint8) * 255
-            )
+        states = (
+            rollout.states[:rollout.step][:, -3:, :, :].cpu().numpy().astype(np.uint8)
+        )
 
-            actions = rollout.actions[:rollout.step].squeeze().cpu().numpy()
-            rewards = rollout.rewards[:rollout.step].squeeze().cpu().numpy()
+        actions = rollout.actions[:rollout.step].squeeze().cpu().numpy()
+        rewards = rollout.rewards[:rollout.step].squeeze().cpu().numpy()
 
-            grids = write_infos(states, rollout, self.params.action_meanings)
+        grids = write_infos(states, rollout, self.params.action_meanings)
 
-            logs["behaviour"] = wandb.Video(states, fps=16, format="gif")
-            logs["behaviour_info"] = wandb.Video(grids, fps=10, format="gif")
-            logs["hist/actions"] = actions
-            logs["hist/rewards"] = rewards
-            logs["mean_reward"] = rewards.mean()
-            logs["episode_length"] = rollout.step
+        logs["behaviour"] = wandb.Video(states, fps=16, format="gif")
+        logs["behaviour_info"] = wandb.Video(grids, fps=10, format="gif")
+        logs["hist/actions"] = actions
+        logs["hist/rewards"] = rewards
+        logs["mean_reward"] = rewards.mean()
+        logs["episode_length"] = rollout.step
 
         if batch_id % self.log_heatmap_step == 0 and len(self.cams) != 0:
 

src/env/NavEnv.py
@@ -113,7 +113,7 @@ class NavEnv(SimpleEnv):
                 observation = np.expand_dims(observation, axis=0)
 
             # normalize observations
-            observation /= 255.0
+            #observation /= 255.0
 
         return observation
 

src/model/FullModel.py
@@ -198,7 +198,10 @@ class FullModel(nn.Module):
             Output of env model :pred_frames, reward_pred, value_pred
 
         """
-        # FIXME: Hardcoded mask, need to fix
+
+        # Normalize Inputs
+        inputs = inputs / 255.0
+
         batch_size = inputs.shape[0]
         #mf_features = self.mf_feature_extractor(inputs, mask)
         #_, action, _ = self.mb_actor.act(inputs, mask)

src/model/ModelFree.py
@@ -210,7 +210,7 @@ class ModelFree(nn.Module):
         action_log_probs = F.log_softmax(action_logit, dim=1)
         entropy = -(action_probs * action_log_probs).sum(1).mean()
 
-        return value, action_log_probs, entropy
+        return value, action_log_probs, entropy, None
 
 
 class NNBase(nn.Module):
@@ -461,7 +461,7 @@ class Conv2DModelFree(nn.Module):
             [batch_size, value]
 
         """
-
+        inputs = inputs / 255.0
         if self.share_weights:
             x = self.feature_extractor.forward(inputs, masks)
             return self.actor(x), self.critic(x)